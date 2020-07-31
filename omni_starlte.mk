246dc5cb6f185f750659a7783fbf8c9f638cdfde
VietNamRom <phongttgalaxy@gmail.com>
2020-07-31 16:04:44 +0700

    Import

---
+++ omni_starlte.mk
@@ -0,0 +1,12 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier
+PRODUCT_DEVICE := starlte
+PRODUCT_NAME := omni_starlte
+PRODUCT_MODEL := Galaxy S9
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
